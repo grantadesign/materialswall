a6d9ac750ca58056300537c130b8fb87c9d2b589
Adrian Banks <github@adrianbanks.co.uk>
2014-01-16 22:39:27 +0000

    style the individual cards

--- DataAccess/Card.cs
+++ DataAccess/Card.cs
@@ -2,11 +2,13 @@
 
 namespace Granta.MaterialsWall.DataAccess
 {
-    public class Card
+    public sealed class Card
     {
         public Guid Identifier{get;set;}
         public string Name{get;set;}
-        public string Description{get;set;}
         public string Path{get;set;}
+        public string Description{get;set;}
+        public string TypicalUses{get;set;}
+        public string Source{get;set;}
     }
 }
