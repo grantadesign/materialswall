b18801d9dd5c98288efd1cd4d378bff0fa194b1f
Adrian Banks <github@adrianbanks.co.uk>
2014-01-17 01:03:41 +0000

    add search filtering to the index page

---
+++ MaterialsWall/Scripts/searchfilter.js
@@ -0,0 +1,15 @@
+﻿function filter(containerClass, searchBox) {
+    var searchText = $(searchBox).val();
+
+    $(containerClass).each(function () {
+        var materialName = $(this).find(".materialname").text();
+        var indexOfSearchText = materialName.search(new RegExp(searchText, "i"));
+
+        if (indexOfSearchText > -1) {
+            $(this).show();
+        }
+        else {
+            $(this).hide();
+        }
+    });
+}
