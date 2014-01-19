86ebc8068d0df54c876135def3d7bdb607d1f099
Adrian Banks <github@adrianbanks.co.uk>
2014-01-19 01:09:41 +0000

    minify all scripts and stylesheets

--- MaterialsWall/App_Start/BundleConfig.cs
+++ MaterialsWall/App_Start/BundleConfig.cs
@@ -7,36 +7,29 @@ namespace Granta.MaterialsWall
         // For more information on Bundling, visit http://go.microsoft.com/fwlink/?LinkId=254725
         public static void RegisterBundles(BundleCollection bundles)
         {
+            bundles.Add(new StyleBundle("~/bundles/labels").Include(
+                        "~/Content/walllabel.css"));
+
+            bundles.Add(new StyleBundle("~/bundles/wallcards").Include(
+                        "~/Content/wallcard.css"));
+
+            bundles.Add(new StyleBundle("~/bundles/cards").Include(
+                        "~/Content/card.css"));
+
+            bundles.Add(new StyleBundle("~/bundles/about").Include(
+                        "~/Content/about.css"));
+
+            bundles.Add(new StyleBundle("~/bundles/css").Include(
+                        "~/Content/site.css",
+                        "~/Content/materialswall.css"));
+
+            bundles.Add(new ScriptBundle("~/bundles/searchfilter").Include(
+                        "~/Scripts/searchfilter.js"));
+
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
                         "~/Scripts/jquery-{version}.js"));
 
-            bundles.Add(new ScriptBundle("~/bundles/jqueryui").Include(
-                        "~/Scripts/jquery-ui-{version}.js"));
-
-            bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
-                        "~/Scripts/jquery.unobtrusive*",
-                        "~/Scripts/jquery.validate*"));
-
-            // Use the development version of Modernizr to develop with and learn from. Then, when you're
-            // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
-            bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
-                        "~/Scripts/modernizr-*"));
-
-            bundles.Add(new StyleBundle("~/Content/css").Include("~/Content/site.css"));
-
-            bundles.Add(new StyleBundle("~/Content/themes/base/css").Include(
-                        "~/Content/themes/base/jquery.ui.core.css",
-                        "~/Content/themes/base/jquery.ui.resizable.css",
-                        "~/Content/themes/base/jquery.ui.selectable.css",
-                        "~/Content/themes/base/jquery.ui.accordion.css",
-                        "~/Content/themes/base/jquery.ui.autocomplete.css",
-                        "~/Content/themes/base/jquery.ui.button.css",
-                        "~/Content/themes/base/jquery.ui.dialog.css",
-                        "~/Content/themes/base/jquery.ui.slider.css",
-                        "~/Content/themes/base/jquery.ui.tabs.css",
-                        "~/Content/themes/base/jquery.ui.datepicker.css",
-                        "~/Content/themes/base/jquery.ui.progressbar.css",
-                        "~/Content/themes/base/jquery.ui.theme.css"));
+            BundleTable.EnableOptimizations = true;
         }
     }
 }
