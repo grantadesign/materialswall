767240d2003489fdc13a723241d6c7536151896d
Adrian Banks <github@adrianbanks.co.uk>
2014-02-01 00:48:38 +0000

    add pagination and infinite scroll to the home page

--- MaterialsWall/Controllers/HomeController.cs
+++ MaterialsWall/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 using System.Web.Mvc;
 using Granta.MaterialsWall.DataAccess;
 
@@ -6,22 +7,32 @@ namespace Granta.MaterialsWall.Controllers
 {
     public sealed class HomeController : Controller
     {
-        private readonly ICardRepository cardRepository;
+        private const int PageSize = 12;
 
-        public HomeController(ICardRepository cardRepository)
+        private readonly ICardRepository cardRepository;
+        private readonly IPaginator<Card> paginator;
+
+        public HomeController(ICardRepository cardRepository, IPaginator<Card> paginator)
         {
             if (cardRepository == null)
             {
                 throw new ArgumentNullException("cardRepository");
             }
+
+            if (paginator == null)
+            {
+                throw new ArgumentNullException("paginator");
+            }
             
             this.cardRepository = cardRepository;
+            this.paginator = paginator;
         }
 
-        public ActionResult Index()
+        public ActionResult Index(int p = 1)
         {
-            var cards = cardRepository.GetCards();
-            return View(cards);
+            var cards = cardRepository.GetCards().ToList();
+            var pagedCards = paginator.GetPage(PageSize, p, cards);
+            return View(pagedCards);
         }
 
         public ActionResult About()
