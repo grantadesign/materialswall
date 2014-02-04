3e2b040adb921e174e1f32d02f7d664fa2b1e691
Adrian Banks <adrian@grantadesign.com>
2014-02-04 11:14:25 +0000

    don't hard-code the url to use in the QR code

--- MaterialsWall/Images/QRCodeGenerator.cs
+++ MaterialsWall/Images/QRCodeGenerator.cs
@@ -8,14 +8,14 @@ namespace Granta.MaterialsWall.Images
 {
     public interface IQRCodeGenerator
     {
-        Bitmap Generate(Guid identifier, int size);
+        Bitmap Generate(string baseUrl, Guid identifier, int size);
     }
 
     public sealed class QRCodeGenerator : IQRCodeGenerator
     {
-        public Bitmap Generate(Guid identifier, int size)
+        public Bitmap Generate(string baseUrl, Guid identifier, int size)
         {
-            string url = "http://materialswall.grantadesign.com/Card/Index/" + identifier;
+            string url = string.Format("{0}QR/{1}", baseUrl, identifier);
             var writer = new BarcodeWriter {Format = BarcodeFormat.QR_CODE};
             var options = new QrCodeEncodingOptions {ErrorCorrection = ErrorCorrectionLevel.L, Height = size, Width = size};
             writer.Options = options;
